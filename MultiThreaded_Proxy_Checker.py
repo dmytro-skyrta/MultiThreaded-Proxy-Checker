5d582adfa07d83926c6775df591507d5655ddc32
dmytro-skyrta <168032357+dmytro-skyrta@users.noreply.github.com>
2024-05-13 14:39:38 +0200

    Update MultiThreaded_Proxy_Checker.py
    Now the connection via Tor is also checked for IP changes

--- MultiThreaded_Proxy_Checker.py
+++ MultiThreaded_Proxy_Checker.py
@@ -4,24 +4,46 @@ import concurrent.futures                                                       
 import threading                                                                                                        # Importing threading for working with threads
 from loguru import logger
 logger.remove()
-                                                                                                                        # Proxy_checker manual settings:
+
+# Proxy_checker manual settings:
 LINK = "https://ipv4.icanhazip.com/"                                                                                    # Site for checking IP
-MAX_THREADS = 100                                                                                                       # Define the maximum number of threads
+MAX_THREADS = 10                                                                                                        # Define the maximum number of threads
 TIMEOUT = 60                                                                                                            # Stop checking proxies after n seconds
 lock = threading.Lock()                                                                                                 # Only one thread works at a time
-write_to_file = "Working_proxies.txt"                                                                                   # File to write working proxies to
-proxy_list_small = "Proxy_list_short.txt"                                                                               # File containing a small list of proxies
-proxy_list_big = "Proxy_list_large.txt"                                                                                 # File containing a big list of proxies
+write_to_file = "02 Working proxies.txt"                                                                                # File to write working proxies to
+proxy_list_small = "03 Proxy list short.txt"                                                                            # File containing a small list of proxies
+proxy_list_big = "04 Proxy list long.txt"                                                                               # File containing a big list of proxies
 
 def check_one_proxy(proxy) :
-    my_proxies = {                                                                                                      # Create a dictionary for the proxy
-        "http": f"http://{proxy}",
-        "https": f"http://{proxy}",
-        "socks4": f"socks4://{proxy}",
-        "socks5": f"socks5://{proxy}"
-    }
-    try :
-        response = requests.get(LINK, proxies=my_proxies, timeout=TIMEOUT)
+    proxy = proxy.lower()
+
+    if "://" in proxy :                                                                                                 # Create a dictionary for the proxy
+        proxy_dict = {
+            "http": f"{proxy}",
+            "https": f"{proxy}",
+            "ftp": f"{proxy}",
+            "ftps": f"/{proxy}",
+            "socks": f"{proxy}",
+            "socks4": f"{proxy}",
+            "socks4a": f"{proxy}",
+            "socks5": f"{proxy}",
+            "socks5h": f"{proxy}"
+            }
+    else :
+        proxy_dict = {
+            "http": f"http://{proxy}",
+            "https": f"http://{proxy}",
+            "ftp": f"ftp://{proxy}",
+            "ftps": f"ftps://{proxy}",
+            "socks": f"socks://{proxy}",
+            "socks4": f"socks4://{proxy}",
+            "socks4a": f"socks4a://{proxy}",
+            "socks5": f"socks5://{proxy}",
+            "socks5h": f"socks5h://{proxy}"
+        }
+
+    try:
+        response = requests.get(LINK, proxies=proxy_dict, timeout=TIMEOUT)
         ip_address = response.text.strip()
         with lock:
             logger.info(f"Proxy is working: {proxy} ; IP: {ip_address}")
@@ -68,7 +90,7 @@ def get_connection_choice() :                                                   
             "Connection choice. You would like to have connection trought:\n"
             "1 = proxy without a check of any lists of proxies \n"
             "2 = proxy with a check of a short list of proxies\n"
-            "3 = proxy with a check of a large list of proxies\n"
+            "3 = proxy with a check of a long list of proxies\n"
             "4 = TOR\n"
             "Make your choice: "
         )
@@ -83,18 +105,18 @@ def get_connection_choice() :                                                   
             write_list_to_file(working_proxies_list, write_to_file)
             return (working_proxies_list)
         elif choice == "3" :
-            logger.info("You chose 3: connection through a proxy with a check of a large list of proxies. Starting the checking procedure: ")
+            logger.info("You chose 3: connection through a proxy with a check of a long list of proxies. Starting the checking procedure: ")
             from_file = proxy_list_big
             working_proxies_list = check_proxy_list(read_list_from_file(from_file))
             write_list_to_file(working_proxies_list, write_to_file)
             return (working_proxies_list)
         elif choice == "4" :
             logger.info("You chose 4: connection through TOR. Starting the connection through TOR:")
-            working_proxies_list = ["socks5://127.0.0.1:9050"]
+            working_proxies_list = check_proxy_list(["socks5://127.0.0.1:9050"])
             return (working_proxies_list)
         else:
             logger.info("Incorrect input. Please choose only 1, 2, or 3.")
 
 if __name__ == "__main__":
-    logger.add(sys.stdout, level="TRACE")                                                                             # Adding logger to output logs to stdout
+    logger.add(sys.stdout, level="TRACE")
     working_proxies_list = get_connection_choice()
